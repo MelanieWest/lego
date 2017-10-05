417caa2540fe16640d57c4e541da33449d50facc
Melanie West <dhmofan@gmail.com>
2017-10-04 22:59:52 -0400

    hard-coded data has been displayed and sorted by the property 'built' being true or false.  Display details need improvement, but the sorting works.  Background image selected and rows and columns set up so that data goes either left or right, depending on status.

--- routes/dataroutes.js
+++ routes/dataroutes.js
@@ -5,14 +5,24 @@ module.exports = function(app){
     app.get("/api/legos",function(req,res){
         //receive data from html fields
         //res.json(legoData);
-        var listItems = {dataArray: []};
+        var listItems = {dataArray: [],doneArray: []};
+
+//listItems go on the left, as 'unbuilt' legos;
+//doneItems go on the right, after being 'built'
+
         for (var i=0; i< legoData.length; i++){
             if(!legoData[i].built){
                 listItems.dataArray.push(legoData[i]);
+            }else{
+                listItems.doneArray.push(legoData[i]);
             }
         }
+
+        // can I 'render' more than one variable at a time?
+
         res.render("index",listItems);
 
+//this is extra output, to test data
         var legoJSON = JSON.stringify(legoData);
         console.log(legoData);
     });     //end of get
