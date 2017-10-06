8bbbf03cd184e01d38722670a7dc46eec246e2df
Melanie West <dhmofan@gmail.com>
2017-10-05 21:32:28 -0400

    Found a simple way to sort the data in handlebars. Still no display, but no errors either.  Console logs are starting to reveal what is happening.  Data is not making it to lego array

--- routes/dataroutes.js
+++ routes/dataroutes.js
@@ -1,41 +1,35 @@
 var legoData = require("../data/legoData.js");
-var listItems = {dataArray: [],doneArray: []};
 
 module.exports = function(app){
 
     app.get("/legos",function(req,res){
         //receive data from html fields
         //res.json(legoData);
+        console.log('app.get');
+        res.render("index",legoData);
 
-        res.render("index",listItems);
-
-//this is extra output, to test data
-        // var legoJSON = JSON.stringify(legoData);
-        // console.log(legoData);
     });     //end of get
 
 
     app.post("/legos", function(req, res) {
 
-//if entering for the first time, set tableData[thisindex].built to false.
-//if the built button was pressed, set tableData[thisindex].built to true 
+        console.log('app.post')
+  //      console.log(req.body);
+        legoData.push(req.body);
+        console.log('lego array: '+legoData);
+        
+            // legoData.id[legoData.length-1] = legoData.length-1;
+            
+            //     for (var i=0; i< legoData.length; i++){
+            //         if(!legoData[i].built){
+            //             listItems.dataArray.push(legoData[i]);
+            //         }else{
+            //             listItems.doneArray.push(legoData[i]);
+            //         }
+            //     }
 
-            console.log(req.body);
-            legoData.push(req.body);
 
-             
-                for (var i=0; i< legoData.length; i++){
-                    if(!legoData[i].built){
-                        listItems.dataArray.push(legoData[i]);
-                    }else{
-                        listItems.doneArray.push(legoData[i]);
-                    }
-                }
-
-        console.log(listItems);
-        res.json(listItems);
-
-         res.render("index",listItems);
+         res.render("index",legoData);
       }); //end of post
 
 };     // end of function
